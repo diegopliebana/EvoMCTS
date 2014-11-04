d31476ea97fb11312467a719d9e05ab74715c742
hruska1 <vodopivec.tom@gmail.com>
2014-11-04 15:04:02 +0000

    Preparation for last batch of experiments in LeftRight

--- src/Test.java
+++ src/Test.java
@@ -51,7 +51,6 @@ public class Test
         //String filename = wkDir.substring(wkDir.lastIndexOf("\\")+1) + ".txt";
 		
         //Game and level to play
-
         int gameIdx = 21;
         int levelIdx = 0; //level names from 0 to 4 (game_lvlN.txt).
         String game = gamesPath + games[gameIdx] + ".txt";
@@ -62,7 +61,7 @@ public class Test
 
         // 2. This plays a game in a level by the controller.
         //ArcadeMachine.runOneGame(game, level1, visuals, sampleMCTSController, recordActionsFile, seed);
-        ArcadeMachine.runOneGame(game, level1, visuals, controller, recordActionsFile, seed);
+        //ArcadeMachine.runOneGame(game, level1, visuals, controller, recordActionsFile, seed);
 
         // 3. This replays a game from an action file previously recorded
         //String readActionsFile = "actionsFile_aliens_lvl0.txt";  //This example is for
@@ -71,15 +70,17 @@ public class Test
         // 4. This plays a single game, in N levels, M times :
         //String level2 = gamesPath + games[gameIdx] + "_lvl" + 1 +".txt";//
 
-        /*int M = 100;
-        int M = 1000;   //number of repeats (only for statistical accuracy)
-        int RLmin = 8;     //rollout lenghts to be tested from RLmin to RLmax, where RLmin >= 1
-        int RLmax = 30;
-        boolean isFixedTest = false;
-        ArcadeMachine.runGamesN(game, level1, M, RLmin, RLmax, controller, isFixedTest, seed, filename);
-        //ArcadeMachine.runGamesN(game, level1, M, RLmin, RLmax, sampleMCTSController, isFixedTest, seed, filename);
-*/
-        
+//        //int M = 100;
+//        int M = 1000;   //number of repeats (only for statistical accuracy)
+//        int RLmin = 8;     //rollout lenghts to be tested from RLmin to RLmax, where RLmin >= 1
+//        int RLmax = 30;
+//        boolean isFixedTest = false;
+//        ArcadeMachine.runGamesN(game, level1, M, RLmin, RLmax, controller, isFixedTest, seed, filename);
+//        //ArcadeMachine.runGamesN(game, level1, M, RLmin, RLmax, sampleMCTSController, isFixedTest, seed, filename);
+
+
+       RightLeft_2014_11_04(gamesPath, games, controller, sampleMCTSController, seed, filename);
+
        /* int M = 100;
         boolean isFixedTest = true;
         RLmin = 1;
@@ -111,5 +112,20 @@ public class Test
             ArcadeMachine.runGames(game, levels, M, controller, saveActions? actionFiles:null, seed);
         }
                      */
+    }
+
+    public static void RightLeft_2014_11_04(String gamesPath, String[] games, String controller, String sampleMCTSController, int seed, String filename){
+
+        int gameIdx = 21;
+        int levelIdx = 0; //level names from 0 to 4 (game_lvlN.txt).
+        String game = gamesPath + games[gameIdx] + ".txt";
+        String level1 = gamesPath + games[gameIdx] + "_lvl" + levelIdx +".txt";
+
+        int M = 1000;   //number of repeats (only for statistical accuracy)
+        int RLmin = 8;     //rollout lenghts to be tested from RLmin to RLmax, where RLmin >= 1
+        int RLmax = 30;
+        boolean isFixedTest = false;
+        ArcadeMachine.runGamesN(game, level1, M, RLmin, RLmax, controller, isFixedTest, seed, filename);
+        ArcadeMachine.runGamesN(game, level1, M, RLmin, RLmax, sampleMCTSController, isFixedTest, seed, filename);
     }
 }
