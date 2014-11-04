e98805e36badb05d1e91382b48bb7b7f1f525e5e
Diego Perez Liebana <akilascartas@gmail.com>
2014-11-04 15:38:23 +0000

    Test change

--- src/Test.java
+++ src/Test.java
@@ -52,17 +52,17 @@ public class Test
 		
         //Game and level to play
 
-        int gameIdx = 21;
-        int levelIdx = 0; //level names from 0 to 4 (game_lvlN.txt).
+        int gameIdx = 20;
+        int levelIdx = 1; //level names from 0 to 4 (game_lvlN.txt).
         String game = gamesPath + games[gameIdx] + ".txt";
         String level1 = gamesPath + games[gameIdx] + "_lvl" + levelIdx +".txt";
 
         // 1. This starts a game, in a level, played by a human.
-        // ArcadeMachine.playOneGame(game, level1, recordActionsFile, seed);
+         //ArcadeMachine.playOneGame(game, level1, recordActionsFile, seed);
 
         // 2. This plays a game in a level by the controller.
         //ArcadeMachine.runOneGame(game, level1, visuals, sampleMCTSController, recordActionsFile, seed);
-        //ArcadeMachine.runOneGame(game, level1, visuals, controller, recordActionsFile, seed);
+        ArcadeMachine.runOneGame(game, level1, visuals, controller, recordActionsFile, seed);
 
         // 3. This replays a game from an action file previously recorded
         //String readActionsFile = "actionsFile_aliens_lvl0.txt";  //This example is for
@@ -72,11 +72,11 @@ public class Test
         //String level2 = gamesPath + games[gameIdx] + "_lvl" + 1 +".txt";//
 
         //int M = 100;
-        int M = 1000;   //number of repeats (only for statistical accuracy)
+      /*  int M = 1000;   //number of repeats (only for statistical accuracy)
         int RLmin = 8;     //rollout lenghts to be tested from RLmin to RLmax, where RLmin >= 1
         int RLmax = 30;
         boolean isFixedTest = false;
-        ArcadeMachine.runGamesN(game, level1, M, RLmin, RLmax, controller, isFixedTest, seed, filename);
+        ArcadeMachine.runGamesN(game, level1, M, RLmin, RLmax, controller, isFixedTest, seed, filename);*/
         //ArcadeMachine.runGamesN(game, level1, M, RLmin, RLmax, sampleMCTSController, isFixedTest, seed, filename);
 
         
